4cda56d759f85cba3d3f47e9e853793013850931
Rick <rick.latrine@gmail.com>
2017-05-06 23:32:16 +0200

    Tuples getestet

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BrickBrackBruck
 {
@@ -7,6 +8,19 @@ namespace BrickBrackBruck
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string[] data = {"aaa", "bbb"};
+            var names = new List<string>(data);
+
+            var tup = (harry:1, stefan:2, derrick:3);
+
+            Console.WriteLine("Tuple 1: " + tup.derrick);                
+            Console.WriteLine("Tuple 2: " + tup.harry);                
+            Console.WriteLine("Tuple 3: " + tup.stefan);                
+
+            foreach (var name in names) {
+                Console.WriteLine("Name: " + name);
+            }
+
         }
     }
 }
